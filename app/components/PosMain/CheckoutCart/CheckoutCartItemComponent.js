ce961ccb4dcc0dd38e7e2c664102b834ba6cc5d9
khanzzirfan <khanzz_irfan@hotmail.com>
2017-05-27 21:51:12 +1200

    fixed the menu checkout cart item. positioning..

--- app/components/PosMain/CheckoutCart/CheckoutCartItemComponent.js
+++ app/components/PosMain/CheckoutCart/CheckoutCartItemComponent.js
@@ -1,36 +1,36 @@
 import React, { Component, PropTypes } from 'react';
 
 class CheckoutCartItemComponent extends Component {
-    constructor(props, context){
+    constructor(props, context) {
         super(props, context);
-     
+
         //local state for each transaction item;
-        this.state={
-            transactionId :0,
-            itemQuantity:0,
-            itemName:'',
-            itemPrice:0.00,
-        } 
+        this.state = {
+            transactionId: 0,
+            itemQuantity: 0,
+            itemName: '',
+            itemPrice: 0.00,
+        }
 
     }
 
     onAddMenuItem = (transactionId) => {
-        console.log(">>> adding item and priting state " );
+        console.log(">>> adding item and priting state ");
         console.log(this.state);
-        
+
         let itemQuantity = this.state.itemQuantity + 1;
         this.setState({
             itemQuantity: itemQuantity
         });
-        
+
         //update the reducer store;
         this.updateTransaction(itemQuantity);
     }
 
-    onRemoveMenuItem = (transactionId) =>{
-        console.log(">>> removing item and priting state " );
+    onRemoveMenuItem = (transactionId) => {
+        console.log(">>> removing item and priting state ");
         console.log(this.state);
-        
+
         let itemQuantity = this.state.itemQuantity - 1;
         this.setState({
             itemQuantity: itemQuantity
@@ -48,7 +48,7 @@ class CheckoutCartItemComponent extends Component {
         this.props.onTransactionUpdate(transactionItem);
     }
 
-    componentDidMount(){
+    componentDidMount() {
         let transactionId = this.props.transactionId;
         let itemPrice = this.props.itemPrice;
         let itemQuantity = this.props.itemQuantity;
@@ -62,18 +62,18 @@ class CheckoutCartItemComponent extends Component {
         });
     }
 
-    componentWillReceiveProps(nextProps){
-         let transactionId = nextProps.transactionId;
-         let itemPrice = nextProps.itemPrice;
-         let itemQuantity = nextProps.itemQuantity;
-         let itemName = nextProps.itemName;
+    componentWillReceiveProps(nextProps) {
+        let transactionId = nextProps.transactionId;
+        let itemPrice = nextProps.itemPrice;
+        let itemQuantity = nextProps.itemQuantity;
+        let itemName = nextProps.itemName;
 
-          this.setState({
-             transactionId: transactionId,
-             itemPrice: itemPrice,
-             itemQuantity: itemQuantity,
-             itemName: itemName
-         });
+        this.setState({
+            transactionId: transactionId,
+            itemPrice: itemPrice,
+            itemQuantity: itemQuantity,
+            itemName: itemName
+        });
     }
 
     render() {
@@ -86,59 +86,33 @@ class CheckoutCartItemComponent extends Component {
         } = this.props;
 
         let formattedName = `${transactionId}. ${itemName}`;
-        let price = `$${this.state.itemPrice}`;        
+        let price = `$${this.state.itemPrice}`;
         let totalAmount = `$${this.state.itemPrice * this.state.itemQuantity}.00`;
 
-        let itemQuantityrowSeperate = {marginTop:"6px"};
-        let itemNamePaddingLeft = {paddingLeft:"0px"};
+        let itemQuantityrowSeperate = { marginTop: "6px" };
+        let itemNamePaddingLeft = { paddingLeft: "0px" };
 
         return (
-            <div>
-                <a className="list-group-item">
-                    <div className="row">
-                        <div className="col-lg-4 text-left">
-                            <span>{transactionId}. {itemName}</span>
-                        </div>
-                         <div className="col-lg-1">
-                            <button type="button" className="btn btn-xs" onClick={this.onAddMenuItem}>
-                                <span className="fa fa-plus-square fa-1x"></span>
-                            </button>
-                        </div>
-                        <div className="col-lg-2 text-center">
-                            <span> x {this.state.itemQuantity} </span>
-                        </div>
-                        <div className="col-lg-1">
-                            <button type="button" className="btn btn-xs" onClick={this.onRemoveMenuItem}>
-                                <span className="fa fa-minus-square fa-1x"></span>
-                            </button>
-                        </div>
-                        <div className="col-lg-2 col-lg-offset-1 text-right">
-                            <span> {totalAmount} </span>
-                        </div>
+            <div className="list-group-item">
+                <div className="row">
+                    <div className="col-lg-5 text-left">
+                        <span>{transactionId}.{itemName}</span>
                     </div>
-                    <div className="row">
-                        <div className="col-xs-1">
-                            <span>Qty</span>
-                        </div>
-                        <div className="col-xs-1">
-                            <button type="button" className="btn btn-xs" onClick={this.onAddMenuItem}>
-                                <span className="fa fa-plus-square fa-1x"></span>
-                            </button>
-                        </div>
-                        <div className="col-xs-1">
-                            <i className="fa fa-1x">{this.state.itemQuantity} </i>
-                        </div>
-                        <div className="col-xs-1">
-                            <button type="button" className="btn btn-xs" onClick={this.onRemoveMenuItem}>
-                                <span className="fa fa-minus-square fa-1x"></span>
-                            </button>
-                        </div>
-                        <div className=" text-right">
-                            <span><i>Amount {totalAmount}</i></span>
-                        </div>
+                    <div className="col-lg-4">
+                        <button type="button" className="btn btn-xs" onClick={this.onAddMenuItem}>
+                            <span className="fa fa-plus-square fa-1x"></span>
+                        </button>
+                        <span> x {this.state.itemQuantity} </span>
+                        <button type="button" className="btn btn-xs" onClick={this.onRemoveMenuItem}>
+                            <span className="fa fa-minus-square fa-1x"></span>
+                        </button>
                     </div>
-                </a>
+                    <div className="col-lg-3 text-right">
+                        <span>{totalAmount}</span>
+                    </div>
+                </div>
             </div>
+
         );
     }
 }
